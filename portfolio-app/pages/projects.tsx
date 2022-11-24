28dc6dd57c2011b767b378479bee6b210fb002a6
aqil07 <aqil.arya07@gmail.com>
2022-11-24 23:58:53 +0200

    updates include project view with materials UI for home icon and some framer animation updates

---
+++ portfolio-app/pages/projects.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+type Props = {}
+
+function Projects({}: Props) {
+  return (
+    <motion.div>
+      <motion.section>
+      
+      </motion.section>
+    </motion.div>
+  )
+}
+
+export default Projects
