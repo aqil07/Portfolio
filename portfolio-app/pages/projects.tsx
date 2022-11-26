6c112a9d7cf36eabd6aff71f35ae538e7fae3920
aqil07 <aqil.arya07@gmail.com>
2022-11-26 21:15:36 +0200

    updates to projects page and global style

--- portfolio-app/pages/projects.tsx
+++ portfolio-app/pages/projects.tsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { motion } from 'framer-motion'
 
 type Props = {}
 
 function Projects({}: Props) {
+  let fill = '#0099ff',
+    fillOpacity = '1',
+    d =
+      'M0,128L48,117.3C96,107,192,85,288,96C384,107,480,149,576,170.7C672,192,768,192,864,186.7C960,181,1056,171,1152,181.3C1248,192,1344,224,1392,240L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z'
+  const svgWave: any = useRef()
+
+  function mouseMove() {
+    let arr = svgWave.current.firstChild.getAttribute('d').split(',')
+    let str;
+    let tmp:any = []
+    arr.map((v: any) => {
+      // console.log(v);
+      if(v.includes('M')){
+        console.log(v.replace('M0','M'+Math.random()))
+        v = v.replace('M0','M'+Math.random())
+      }else if(v.includes('L')){
+        v = v.replace(/L\g/,'L'+Math.random())
+      }
+      
+      tmp.push(v)
+    })
+    str = tmp.toString()
+    svgWave.current.firstChild.setAttribute('d',str) 
+    
+    console.log(svgWave.current.firstChild.getAttribute('d'))
+    // console.log(tmp);
+    
+  }
   return (
     <motion.div>
       <motion.section>
-      
+        <svg
+          ref={svgWave}
+          onMouseMove={mouseMove}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+        >
+          <path fill={fill} fillOpacity={fillOpacity} d={d}></path>
+        </svg>
       </motion.section>
     </motion.div>
   )
